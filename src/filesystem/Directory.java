55f54dd79e38ac2f6936580b6ce388dab38a250e
Maxime Samyn <maxime.harold.07@gmail.com>
2026-03-27 14:08:36 +0100

    remove unnecessary class

--- src/filesystem/Directory.java
+++ src/filesystem/Directory.java
@@ -1,4 +1,19 @@
 package filesystem;
 
+import java.util.ArrayList;
+
 public class Directory {
+
+    private String name;
+    private String path;
+    private ArrayList<File> files;
+    private ArrayList<Link> links;
+    private ArrayList<Directory> directories;
+
+
+
+    //public void updateDirectoryOrder()
+
+    //public void sortDirectory
+
 }
